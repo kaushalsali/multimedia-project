bd4d59a090f8131005d9297b6271c9cab3037959
Kaushal Sali <salikaushal@gmail.com>
2020-05-07 20:30:50 -0400

    Interaction with sketch will work only after pressing 'close' button in the modal.

--- project/src/js/sketch.js
+++ project/src/js/sketch.js
@@ -3,12 +3,9 @@ $(window).on('load',function(){
 });
 
 $(function() {
-    $('#btn-modal-start').on('click', async () => {
-        await Tone.start();
-        console.log('audio is ready');
-
-        setup2();
-
+    $('#btn-modal-start').click(() => {
+        Tone.start();
+        isStarted = true; // Start interaction handling and drawing.
     });
 });
 
@@ -17,10 +14,9 @@ $(function() {
 let socket = io.connect();
 let userNodeCount = 0;
 
-
 // State
+let isStarted = false;
 let isPlaying = false;
-
 
 // Tone
 let masterEnv;
@@ -115,15 +111,16 @@ function draw() {
 
 // Overloads mousePressed of p5.
 function mousePressed() {
-    let nodes = nodeManager.getAllNodes();
-    for (let node of nodes) {
-        let correctedNodeX = ((node.x - width/2) * viewScale + width/2) + (viewOffsetX * viewScale) ;
-        let correctedNodeY = ((node.y - height/2) * viewScale + height/2) + (viewOffsetY * viewScale);
-        let distance = dist(mouseX, mouseY, correctedNodeX, correctedNodeY);
-        if (distance < NODE_SIZE * viewScale) { // Node selected
-            nodeManager.setSelectedNode(node.getId());
-            selSynth.selected(node.getSynthName());
-
+    if (isStarted) {
+        let nodes = nodeManager.getAllNodes();
+        for (let node of nodes) {
+            let correctedNodeX = ((node.x - width / 2) * viewScale + width / 2) + (viewOffsetX * viewScale);
+            let correctedNodeY = ((node.y - height / 2) * viewScale + height / 2) + (viewOffsetY * viewScale);
+            let distance = dist(mouseX, mouseY, correctedNodeX, correctedNodeY);
+            if (distance < NODE_SIZE * viewScale) { // Node selected
+                nodeManager.setSelectedNode(node.getId());
+                selSynth.selected(node.getSynthName());
+            }
         }
     }
 }
@@ -133,7 +130,9 @@ function mousePressed() {
  * Controls view scaling.
  */
 function mouseWheel(event) {
-    viewScale = Math.min(Math.max(viewScale + event.delta * VIEW_SCALE_FACTOR, VIEW_SCALE_MIN), VIEW_SCALE_MAX);
+    if (isStarted) {
+        viewScale = Math.min(Math.max(viewScale + event.delta * VIEW_SCALE_FACTOR, VIEW_SCALE_MIN), VIEW_SCALE_MAX);
+    }
 }
 
 
@@ -141,19 +140,21 @@ function mouseWheel(event) {
  *  Handles translation of the canvas. Updates viewOffsetX and viewOffsetY.
  */
 function updateViewTranslationParameters() {
-    if (mouseX === 0 && mouseY === 0) // Hack to avoid translation when page is reloaded.
-        return;
-    if (mouseX > width - VIEW_TRANSLATION_MARGIN) { // right
-        viewOffsetX = Math.max(view_min_x_offset, viewOffsetX-VIEW_TRANSLATION_SPEED);
-    }
-    if (mouseX < VIEW_TRANSLATION_MARGIN) { //left
-        viewOffsetX = Math.min(view_max_x_offset, viewOffsetX+VIEW_TRANSLATION_SPEED);
-    }
-    if (mouseY > height-VIEW_TRANSLATION_MARGIN) { // bottom
-        viewOffsetY = Math.max(view_min_y_offset, viewOffsetY-VIEW_TRANSLATION_SPEED);
-    }
-    if (mouseY < VIEW_TRANSLATION_MARGIN) { // top
-        viewOffsetY = Math.min(view_max_y_offset, viewOffsetY+VIEW_TRANSLATION_SPEED);
+    if (isStarted) {
+        if (mouseX === 0 && mouseY === 0) // Hack to avoid translation when page is reloaded.
+            return;
+        if (mouseX > width - VIEW_TRANSLATION_MARGIN) { // right
+            viewOffsetX = Math.max(view_min_x_offset, viewOffsetX - VIEW_TRANSLATION_SPEED);
+        }
+        if (mouseX < VIEW_TRANSLATION_MARGIN) { //left
+            viewOffsetX = Math.min(view_max_x_offset, viewOffsetX + VIEW_TRANSLATION_SPEED);
+        }
+        if (mouseY > height - VIEW_TRANSLATION_MARGIN) { // bottom
+            viewOffsetY = Math.max(view_min_y_offset, viewOffsetY - VIEW_TRANSLATION_SPEED);
+        }
+        if (mouseY < VIEW_TRANSLATION_MARGIN) { // top
+            viewOffsetY = Math.min(view_max_y_offset, viewOffsetY + VIEW_TRANSLATION_SPEED);
+        }
     }
 }
 
@@ -369,87 +370,63 @@ function drawViewRect() {
 }
 
 document.addEventListener('keydown', function(event) {
-
-    if (event.keyCode === 65) { // A
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"C4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "C4"});
-    }
-    else if (event.keyCode === 83) { // S
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"D4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "D4"});
-    }
-    else if (event.keyCode === 68) { // D
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"E4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "E4"});
-    }
-    else if (event.keyCode === 70) { // F
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"F4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "F4"});
-    }
-    else if (event.keyCode === 71) { // G
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"G4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "G4"});
-    }
-    else if (event.keyCode === 72) { // H
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"A4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "A4"});
-    }
-    else if (event.keyCode === 74) { // J
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"B4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "B4"});
-    }
-    else if (event.keyCode === 75) { // K
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"C5");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "C5"});
-    }
-    else if (event.keyCode === 76) { // L
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"D5");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "D5"});
-    }
-    else if (event.keyCode === 186) { // ;
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"E5");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "E5"});
-    }
-    else if (event.keyCode === 87) { // W
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"C#4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "C#4"});
-    }
-    else if (event.keyCode === 69) { // E
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"D#4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "D#4"});
-    }
-    else if (event.keyCode === 84) { // T
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"F#4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "F#4"});
-    }
-    else if (event.keyCode === 89) { // Y
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"G#4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "G#4"});
-    }
-    else if (event.keyCode === 85) { // U
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"A#4");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "A#4"});
-    }
-    else if (event.keyCode === 79) { // O
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"C#5");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "C#5"});
-    }
-    else if (event.keyCode === 80) { // P
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),"D#5");
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "D#5"});
-    }
-    else if (event.keyCode === 32) { // SPACE
-        nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(),null); // Rest
-        socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: null});
-    }
-
-    if (!isPlaying) { // Not playing //TODO: This functionality is not necessary
-        if (event.keyCode === 188) {     // ,
-            nodeManager.getNode(nodeManager.getSelectedNodeId()).stepBackward();
+    if (isStarted) {
+        if (event.keyCode === 65) { // A
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "C4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "C4"});
+        } else if (event.keyCode === 83) { // S
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "D4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "D4"});
+        } else if (event.keyCode === 68) { // D
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "E4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "E4"});
+        } else if (event.keyCode === 70) { // F
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "F4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "F4"});
+        } else if (event.keyCode === 71) { // G
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "G4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "G4"});
+        } else if (event.keyCode === 72) { // H
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "A4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "A4"});
+        } else if (event.keyCode === 74) { // J
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "B4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "B4"});
+        } else if (event.keyCode === 75) { // K
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "C5");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "C5"});
+        } else if (event.keyCode === 76) { // L
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "D5");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "D5"});
+        } else if (event.keyCode === 186) { // ;
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "E5");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "E5"});
+        } else if (event.keyCode === 87) { // W
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "C#4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "C#4"});
+        } else if (event.keyCode === 69) { // E
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "D#4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "D#4"});
+        } else if (event.keyCode === 84) { // T
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "F#4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "F#4"});
+        } else if (event.keyCode === 89) { // Y
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "G#4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "G#4"});
+        } else if (event.keyCode === 85) { // U
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "A#4");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "A#4"});
+        } else if (event.keyCode === 79) { // O
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "C#5");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "C#5"});
+        } else if (event.keyCode === 80) { // P
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), "D#5");
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: "D#5"});
+        } else if (event.keyCode === 32) { // SPACE
+            nodeManager.addSampleToUserNode(nodeManager.getSelectedNodeId(), null); // Rest
+            socket.emit('add-sample-to-user-node', {node: nodeManager.getSelectedNodeId(), note: null});
         }
-        if (event.keyCode === 190) {     // . STEP
-            nodeManager.getNode(nodeManager.getSelectedNodeId()).stepForward();
-        }
+
     }
 });
 
